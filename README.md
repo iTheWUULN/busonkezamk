0a9113b6ad8547b21334704a2e7178c155a3ea30
iTheWUULN <96634321+iTheWUULN@users.noreply.github.com>
2024-03-28 00:04:26 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# busonkezamk
+# discordjs-v13-ticket-bot
+Discord sunucularınıza özel kendi kodladığım ticket botum.
---
+++ source/Configurations/Client_Settings.js
@@ -0,0 +1,11 @@
+module.exports = {
+    botToken: "MTIyMjE5MTUyMDUyMjk2MDkzNw.GQh-KN.EP8kJ04E3HEPUR1xQaEejJUgV_4hkaR4j4sR7k",
+    clientOwner: ["795204246046769182"],
+    prefix: ["."],
+
+    voiceChannel: "1222645793605947522",
+
+    clientPresences: ["Salorant"],
+    clientActivity: "Salorant",
+    activityType: "STREAMING"
+ }
